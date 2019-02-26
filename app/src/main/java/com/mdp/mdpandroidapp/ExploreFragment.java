f2475b204842e847c95c6d248c55be7d8b87f1a7
jayden <goldenjayden@gmail.com>
2019-02-26 14:55:43 +0800

    frag_explore
    j

--- app/src/main/java/com/mdp/mdpandroidapp/ExploreFragment.java
+++ app/src/main/java/com/mdp/mdpandroidapp/ExploreFragment.java
@@ -19,11 +19,23 @@ public class ExploreFragment extends Fragment {
         // Required empty public constructor
     }
 
+    public class Box {
+        private Button click_box;
+        String grid_status;
+        int row, col;
+
+        private Box() {
+            grid_status = "unoccupied";
+            row = 1;
+            col = 1;
+        }
+    }
+    private Box box = new Box();
+
     // waypoint button and startpoint button
     private Button waypoint_button;
     private Button startpoint_button;
-    private TextView click_test;
-    private TextView textview_status;
+    private TextView button_status;
     int waypoint_mode, startpoint_mode;
 
     @Override
@@ -38,14 +50,20 @@ public class ExploreFragment extends Fragment {
         // instantiate the two buttons, clickable, and status
         waypoint_button = exploreView.findViewById(R.id.waypoint_button);
         startpoint_button = exploreView.findViewById(R.id.startpoint_button);
-        click_test = exploreView.findViewById(R.id.click_test);
-        textview_status = exploreView.findViewById(R.id.textview_status);
+        box.click_box = exploreView.findViewById(R.id.click_test);
+        button_status = exploreView.findViewById(R.id.button_status);
+        button_status.setText("none");
 
         // setonclicklistener waypoint
         waypoint_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                set("waypoint");
+                if (waypoint_mode == 0) {
+                    set_mode("waypoint");
+                }
+                else {
+                    set_mode("neither");
+                }
             }
         });
 
@@ -53,62 +71,82 @@ public class ExploreFragment extends Fragment {
         startpoint_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                set("startpoint");
+                if (startpoint_mode == 0) {
+                    set_mode("startpoint");
+                }
+                else {
+                    set_mode("neither");
+                }
             }
         });
 
         // setonclicklistener click_test
-        click_test.setOnClickListener(new View.OnClickListener() {
+        box.click_box.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // if textview_status == unoccupied
-                    // if waypoint_mode == 1
-                        // mark this as waypoint
-                        // save waypoint coordinates
-                        // waypoint_mode = 0
-                    // elif startpoint_mode == 1
-                        // mark this as startpoint
-                        // save startpoint coordinates
-                        // startpoint_mode = 0
+                if (box.grid_status == "unoccupied") {
+                    if (waypoint_mode == 1){
+                        box.grid_status = "waypoint";
+                        set_coord(box.row, box.col, "waypoint");
+                    }
+                    else if (startpoint_mode == 1){
+                        box.grid_status = "startpoint";
+                        set_coord(box.row, box.col, "startpoint");
+                    }
+                }
 
-                // if textview_status == waypoint | textview_status == startpoint
-                    // if textview_status == waypoint
-                        // if waypoint_mode == 1
-                            // do nothing
-                        // if startpoint_mode == 1
-                            // mark this as startpoint
-                            // save startpoint coordinates
-                            // remove waypoint coordinates
+                else if (box.grid_status == "waypoint"){
+                    if (waypoint_mode == 1){
+                        // do nothing
+                    }
+                    else if (startpoint_mode == 1){
+                        box.grid_status = "startpoint";
+                        set_coord(box.row, box.col, "startpoint");
+                        unset_coord("waypoint");
+                    }
+                }
 
-                    // if textview_status == startpoint
-                        // if startpoint_mode == 1
-                            // do nothing
-                        // if waypoint_mode == 1
-                            // mark this as waypoint
-                            // save waypoint coordinates
-                            // remove startpoint coordinates
+                else if (box.grid_status == "startpoint"){
+                    if (startpoint_mode == 1){
+                        // do nothing
+                    }
+                    else if (waypoint_mode == 1){
+                        box.grid_status = "waypoint";
+                        set_coord(box.row, box.col, "waypoint");
+                        unset_coord("startpoint");
+                    }
+                }
 
                 // send coordinates and info to RPi
             }
         });
 
         return exploreView;
-//        return inflater.inflate(R.layout.fragment_explore, container, false);
     }
 
-    public void set(String point) {
+    public void set_mode(String point) {
         if (point == "waypoint") {
             startpoint_mode = 0;
             waypoint_mode = 1;
-            textview_status.setText("waypoint mode");
-            Log.d("setbutton", "waypoint set");
+            button_status.setText("waypoint mode");
         }
         else if (point == "startpoint"){
             waypoint_mode = 0;
             startpoint_mode = 1;
-            textview_status.setText("startpoint mode");
-            Log.d("setbutton", "startpoint set");
+            button_status.setText("startpoint mode");
+        }
+        else if (point == "neither"){
+            waypoint_mode = 0;
+            startpoint_mode = 0;
+            button_status.setText("none");
         }
     }
 
+    public void set_coord(int row, int col, String point){
+        Log.d("set coordinates", "coordinates for " + point + " set!");
+    }
+
+    public void unset_coord(String point){
+        Log.d("unset coordinates", "coordinates for " + point + " unset!");
+    }
 }
